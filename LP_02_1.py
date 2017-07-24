3774527b2fabadb892422f4ff427dbb7c6a633b6
Dimulkap <Dimulka-p@yandex.ru>
2017-07-24 16:36:07 +0300

    Завершение.

--- LP_02_1.py
+++ LP_02_1.py
@@ -1,7 +1,10 @@
-budget = 1000000000
+budget = 2000000000
 goverment = 200000000
 army = 300000000
 education = 50000000
 pensions = 50000000
 roads_and_fools = 200000000
 nanotechnologies = 300000000
+expenses_total = goverment + army + education + pensions + roads_and_fools + nanotechnologies
+balance = budget - expenses_total
+print(balance)
